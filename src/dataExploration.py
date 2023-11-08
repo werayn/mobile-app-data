4c44dd9160e3f2edebdef422b8c57e4150dbd997
TARIFA Salma <salma.tarifa@sacem.fr>
2023-11-08 14:46:18 +0100

    affichage des colonnes et leur types et voir les colonnes vides/nulles

--- src/dataExploration.py
+++ src/dataExploration.py
@@ -4,7 +4,24 @@ df_apple=pd.read_csv("data/apple/appList.csv")
 df_google=pd.read_csv("data/google/appList.csv")
 
 
-print(df_apple.head())
-print(df_apple.isnull.sum())
+
+merged_df=pd.concat([df_apple,df_google])
+
+
+#display columns and their types
+print(merged_df.dtypes)
+
+
+#display null or empty columns 
+print(merged_df.columns[merged_df.isnull().all()])
+      
+
+
+
+
+
+
+#print(df_apple.head())
+#print(df_apple.isnull.sum())
 
 
